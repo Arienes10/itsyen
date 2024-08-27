08e2ba77619c9d1f5818a6b8034c8da3f6875e9f
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2024-08-27 22:48:16 +0800

    Update rank.lua

--- rank.lua
+++ rank.lua
@@ -3,7 +3,7 @@ script_key="raIKxhkYjtYOIBenbfiZUMiFjLPGwQyy";
 -- If not sure ask info in channel
 _G.GPROGRESS_MODE = "Hybrid"
 _G.GRANK_TO = 99
-_G.GZONE_TO = 224 -- ONLY increase above 99 to go to world2, only when 100% sure, there is no way back for the "best zone" quests etc. 
+_G.GZONE_TO = 999 -- ONLY increase above 99 to go to world2, only when 100% sure, there is no way back for the "best zone" quests etc. 
 _G.GMAX_EGG_SLOTS = 25
 _G.GMAX_EQUIP_SLOTS = 80
 _G.GMAX_ZONE_UPGRADE_COST = 20000
