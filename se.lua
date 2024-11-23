062609c16931ce92104a79a721f627943aad63b0
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2024-11-24 03:16:30 +0800

    Update se.lua

--- se.lua
+++ se.lua
@@ -1,38 +1,38 @@
--- getgenv().Settings = {        
---     FPSLimit = 60,
---     ["Notifications"] = {
---         Webhook = "https://discord.com/api/webhooks/1296302108727050301/aMZDCH_4Mdpx4vGHHIdJ2J1jVUZl1IoWqFuG4_lPg02N9b6zRMYnHFELBA3MCJ0vTQgM",
---         DiscordID = "622997465607700510",
---         Difficulty = "Above 1m",
---         Rarities = {"Divine", "Exotic", "Secret", "Exclusive", "Celestial", "Superior"},
---         --// Difficulty & Rarity don't work together, customize how you feel. \\--
---     },
---     ["Mailing"] = {
---         Usernames = {"TisIyen"},
---         ["Pets"] = {
---             KeepBestPets = false,
---             Difficulty = "Above 25m",
---             Rarities = {"Divine", "Exotic", "Secret", "Exclusive", "Celestial", "Superior", "Mythical"},
---             --// Difficulty & Rarity don't work together, customize how you feel. \\--
---         },
---         ["Misc"] = {
---             ["Send Instant Luck 4"] = {Active = true, Minimum = 1},
---             ["Send Exclusive Fishing Items"] = {Active = true, Minimum = 1},
---             ["Send Keys"] = {
---                 ["Crystal"] = {Active = true, Minimum = 5, Parts = {UpperMinimum = 20, LowerMinimum = 0}},
---                 ["Secret"] = {Active = true, Minimum = 5},
---             },
---             --// Only the first account listed will receive these items. \\--
---         }
---     }
--- }
--- loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/957ebb42504c2c23a15c8e78a4758f38.lua"))()
+getgenv().Settings = {        
+    FPSLimit = 60,
+    ["Notifications"] = {
+        Webhook = "https://discord.com/api/webhooks/1296302108727050301/aMZDCH_4Mdpx4vGHHIdJ2J1jVUZl1IoWqFuG4_lPg02N9b6zRMYnHFELBA3MCJ0vTQgM",
+        DiscordID = "622997465607700510",
+        Difficulty = "Above 1m",
+        Rarities = {"Divine", "Exotic", "Secret", "Exclusive", "Celestial", "Superior"},
+        --// Difficulty & Rarity don't work together, customize how you feel. \\--
+    },
+    ["Mailing"] = {
+        Usernames = {"TisIyen"},
+        ["Pets"] = {
+            KeepBestPets = false,
+            Difficulty = "Above 25m",
+            Rarities = {"Divine", "Exotic", "Secret", "Exclusive", "Celestial", "Superior", "Mythical"},
+            --// Difficulty & Rarity don't work together, customize how you feel. \\--
+        },
+        ["Misc"] = {
+            ["Send Instant Luck 4"] = {Active = true, Minimum = 1},
+            ["Send Exclusive Fishing Items"] = {Active = true, Minimum = 1},
+            ["Send Keys"] = {
+                ["Crystal"] = {Active = true, Minimum = 5, Parts = {UpperMinimum = 20, LowerMinimum = 0}},
+                ["Secret"] = {Active = true, Minimum = 5},
+            },
+            --// Only the first account listed will receive these items. \\--
+        }
+    }
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/957ebb42504c2c23a15c8e78a4758f38.lua"))()
 
-script_key = "OFSIayPMiDwtZOiHngjLsarLnIqbAclq";
-_G.GDO_LINE_FOR_HUGE = true
-_G.GGFX_MODE = 1
-_G.GZONE_TO = 99 -- to enter event from W3 intead put 999
-_G.GWEBHOOK_USERID = "622997465607700510"
-_G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1296302108727050301/aMZDCH_4Mdpx4vGHHIdJ2J1jVUZl1IoWqFuG4_lPg02N9b6zRMYnHFELBA3MCJ0vTQgM"
-_G.GMAIL_RECEIVERS = {"imjustyou_3"}
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
+-- script_key = "OFSIayPMiDwtZOiHngjLsarLnIqbAclq";
+-- _G.GDO_LINE_FOR_HUGE = true
+-- _G.GGFX_MODE = 1
+-- _G.GZONE_TO = 99 -- to enter event from W3 intead put 999
+-- _G.GWEBHOOK_USERID = "622997465607700510"
+-- _G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1296302108727050301/aMZDCH_4Mdpx4vGHHIdJ2J1jVUZl1IoWqFuG4_lPg02N9b6zRMYnHFELBA3MCJ0vTQgM"
+-- _G.GMAIL_RECEIVERS = {"imjustyou_3"}
+-- loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
