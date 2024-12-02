6fc45d32c4aa59af92e00c2f37dbbd5b721c4723
Arienes10 <56785786+Arienes10@users.noreply.github.com>
2024-12-02 10:44:57 +0800

    Create aut.lua

---
+++ aut.lua
@@ -0,0 +1,14 @@
+script_key = "OFSIayPMiDwtZOiHngjLsarLnIqbAclq";
+_G.GDO_AUTUMN_EVENT = true
+_G.GHATCH_CHARGED_EGGS = true -- these cost 20 times more!
+_G.GHATCH_GOLDEN_EGGS = false -- these cost 50 times more! you can turn to true to test if you can sustain.
+_G.GPOTIONS = {"Coins","Lucky","The Cocktail","Treasure Hunter","Walkspeed","Diamonds","Damage"}
+_G.GENCHANTS = {"Lucky Eggs","Lucky Eggs","Tap Power", "Coins","Coins","Treasure Hunter", "Strong Pets", "Criticals"}
+_G.GGFX_MODE = 1
+_G.GZONE_TO = 99 -- to enter event from W3 intead put 999
+_G.GWEBHOOK_USERID = "622997465607700510"
+_G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1251137383018270752/5TBEtDTLgoes7TWnsvBwpoVjJgmia6_yQRxJQ-n08kACLJdwnSIpj914wXDW9w3bhpcB"
+_G.GMAIL_RECEIVERS = {"GamingIver15"}
+_G.GMAIL_NORMAL_AUTUMN_TEDDY_BEAR_MIN = 100 -- you can choose your own minimum value, if you have more it will send all.
+_G.GMAIL_NORMAL_AUTUMN_CHEST_MIMIC_MIN = 100 -- same 
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
